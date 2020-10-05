80444dc872eb4089708c7f0f09c5fc0d06e54c38
“Mineliegoma” <garlandegoma@gmail.com>
2020-10-05 18:32:18 -0400

    ex6 state handling

--- my_practices/src/Header.js
+++ my_practices/src/Header.js
@@ -11,12 +11,14 @@ class Header extends React.Component {
  // EX4,EX5
  // think of it as initialize in ruby, its a method that will run first every single time an instance of this classe is being create 
  // if there is any code that you want to render before anything else you should put it inside the constructor
- constructor(props) {
-  super(props)
-  this.state = { color: "orange" } // im creating an object that i will use inside of this componant to manage its state 
-  //console.log("console log in constructor")
+ // constructor(props) {
+ //  super(props)
+ //  this.state = { color: "violet" } // im creating an object that i will use inside of this componant to manage its state 
+ //  //console.log("console log in constructor")
 
- }
+ // }
+ //another way to do this is to create, this is ex6 
+ state = { color: "red" }
  instructors = () => {
   return this.props.instructors.map(function (instructor_name) { return <Bline name={instructor_name} /> })
  }
