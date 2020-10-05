85b2cb82dfef1a5ab6f72c4896021a3c66229837
“Mineliegoma” <garlandegoma@gmail.com>
2020-10-05 18:29:09 -0400

    use of state using constructor

--- my_practices/src/Header.js
+++ my_practices/src/Header.js
@@ -13,17 +13,18 @@ class Header extends React.Component {
  // if there is any code that you want to render before anything else you should put it inside the constructor
  constructor(props) {
   super(props)
-  console.log("console log in constructor")
+  this.state = { color: "orange" } // im creating an object that i will use inside of this componant to manage its state 
+  //console.log("console log in constructor")
 
  }
  instructors = () => {
   return this.props.instructors.map(function (instructor_name) { return <Bline name={instructor_name} /> })
  }
  render() {
-  console.log("console log in render method")
+  console.log("console log in render method:", this.state)
   return (
    <div>
-    <h1> Welcome to our app</h1>
+    <h1 style={{ "border-style": "dashed", "border-color": `${this.state.color}` }}> Welcome to our app</h1>
     {this.instructors()}
 
    </div>
