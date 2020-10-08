aa76c3d01208d8de09da890384dca6f1ea9fb135
“Mineliegoma” <garlandegoma@gmail.com>
2020-10-08 16:11:09 -0400

    form created

--- my_practices/src/Containers/InstructorContainer.js
+++ my_practices/src/Containers/InstructorContainer.js
@@ -6,17 +6,30 @@ import Form from '../components/Form'
 
 
 class InstructorContainer extends React.Component {
+ state = {
+  instructors: [{ id: 1, name: "Tashawn" }, { id: 2, name: "greg" }, { id: 3, name: "Steven" }, { id: 4, name: "Ian" }]
+ }
 
- instructors_names = () => [{ id: 1, name: "Tashawn" }, { id: 2, name: "greg" }, { id: 3, name: "Steven" }, { id: 4, name: "Ian" }]
+
+
+ submitHandler = (element) => {
+  // create a copy of the arrray api
+  let myArray = [...this.state.instructors, element]
+  console.log('old state:', this.state.instructors)
+
+  this.setState({ instructors: myArray })
+
+ }
+
 
 
  render() {
 
-  let instructors = this.instructors_names().map(instructorObject => <Instructor key={instructorObject.id} instructor={instructorObject} appClickHandler={this.props.appClickHandler} />)
+  let instructors = this.state.instructors.map(instructorObject => <Instructor key={instructorObject.id} instructor={instructorObject} appClickHandler={this.props.appClickHandler} />)
   //this instructor singular should match the instructor component see line 3 instructor.js 
   return (
    <>
-    <Form />
+    <Form submitHandler={this.submitHandler} />
     { instructors}
    </>
   )
--- my_practices/src/components/Form.js
+++ my_practices/src/components/Form.js
@@ -1,19 +1,31 @@
 import React from "react"
 
-function Form() {
- return (
+class Form extends React.Component {
 
-  <form onSubmit={(e) => {
-   e.preventDefault()
-   let name = e.target[0].value
-   console.log(name)
-  }}
-  >
-   <input type="text" placeholder="Enter instructor name" />
-   <input type="submit" value="create instructor" />
-  </form>
+ state = {
+  name: ""
+ }
+ changeHandler = (e) => {
+  //console.log('events:', e.target.value)
+  this.setState({ name: e.target.value })
 
- )
+ }
+
+ render() {
+  return (
+
+   <form onSubmit={(e) => {
+    e.preventDefault()
+    this.props.submitHandler({ name: e.target[0].value })
+    //console.log(name)
+   }}
+   >
+    <input type="text" placeholder="Enter instructor name" value={this.state.name} onChange={this.changeHandler} />
+    <input type="submit" value="create instructor" />
+   </form>
+
+  )
+ }
 
 }
 export default Form
